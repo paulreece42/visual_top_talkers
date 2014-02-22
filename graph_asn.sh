b21b167f1efe3be952433382dfb5ea110cdd611a
Paul Reece <paul@reece.cc>
2014-02-21 21:27:11 -0500

    bugfixes, graph going wrong way and wasnt filtering out same-as traffic

--- graph_asn.sh
+++ graph_asn.sh
@@ -10,8 +10,8 @@ output_out="/tmp/out.txt"
 rm -rf $output_in
 rm -rf $output_out
 
-RESULT_OUT=`psql -U pmacct -t -c "SELECT SUM(bytes) * 8 FROM $MY_TABLE WHERE as_dst = $1 AND as_src = $MY_ASN GROUP BY stamp_inserted ORDER BY stamp_inserted LIMIT 2016;"`
-RESULT_IN=`psql -U pmacct -t -c "SELECT SUM(bytes) * 8 FROM $MY_TABLE WHERE as_src = $1 AND as_dst= $MY_ASN GROUP BY stamp_inserted ORDER BY stamp_inserted LIMIT 2016;"`
+RESULT_OUT=`psql -U pmacct -t -c "SELECT SUM(bytes) * 8 FROM $MY_TABLE WHERE as_dst = $1 AND as_src = $MY_ASN GROUP BY stamp_inserted ORDER BY stamp_inserted DESC LIMIT 2016;"`
+RESULT_IN=`psql -U pmacct -t -c "SELECT SUM(bytes) * 8 FROM $MY_TABLE WHERE as_src = $1 AND as_dst = $MY_ASN GROUP BY stamp_inserted ORDER BY stamp_inserted DESC LIMIT 2016;"`
 
 j=0
 for i in $RESULT_IN
